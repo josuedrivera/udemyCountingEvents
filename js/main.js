6bf8f921a29110034abc1699a9c95282319f080c
JOSUE RIVERA <josuerivera@JOSUEs-MacBook-Air.local>
2019-12-07 23:46:26 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("connected");
+
+//type these commands into the console
+document.querySelectorAll("table");
+document.querySelectorAll("tr");
+document.querySelectorAll("tr").length - document.querySelectorAll("table").length;
+
+//what ever returns is the amount of events on the page
+//ref lecture 194: Counting Events Exercise
